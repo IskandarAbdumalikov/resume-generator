6139bbe555a79091f19ede420e93984d722d38c9
abdullokhpro <abdulloxrasulov441@gmail.com>
2024-06-26 21:57:18 +0500

    asdf

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./admin.scss";
+import { Outlet } from "react-router-dom";
+import AdminHeader from "./admin-header/AdminHeader";
+import Sidebar from "./sidebar/Sidebar";
+// import { useGetUsersQuery } from "../../context/userSlice";
 
 const Admin = () => {
+  const [close, setClose] = useState(false);
+  // let { data } = useGetUsersQuery();
+  // console.log(data);
   return (
-    <div>Admin</div>
-  )
-}
+    <>
+      <div className={`admin ${close ? "close" : ""}`}>
+        <Sidebar />
+        <div>
+          <AdminHeader setClose={setClose} />
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Admin
+export default Admin;
